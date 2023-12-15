cc0d1c8af0ac2d0f5496f0f44e565beec6f3441b
TwoCanOfTuna <twocanoftuna@gmail.com>
2023-12-15 01:16:04 -0500

    Changed github mod file download method

--- Install-Mods.ps1
+++ Install-Mods.ps1
@@ -184,12 +184,8 @@ function Install ($arguments) {
 
     # Download and install helmetcamera
     Write-Host "Downloading and installing HelmetCamera"
-    $hcUrl = "https://github.com/TwoCanOfTuna/company-mods-install/releases/download/company-mods-install3/HelmetCamera.dll"
-    $hcStream = Request-Stream $hcUrl
-    $hcPath = Join-Path $lethalCompanyPath "BepInEx/plugins"
-    Expand-Stream $hcStream $hcPath
-    Write-Host "Installed HelmetCamera"
-    Write-Host ""
+    $hcPath = Join-Path $lethalCompanyPath "BepInEx/plugins/HelmetCamera.dll/"
+    powershell -Command Invoke-WebRequest https://github.com/TwoCanOfTuna/company-mods-install/releases/download/company-mods-install3/HelmetCamera.dll -OutFile $hcPath
 }
 
 try {
