a3e5815147ebffa8361dbcc9a5f3cc42d2ac7f60
TwoCanOfTuna <twocanoftuna@gmail.com>
2023-12-15 01:17:43 -0500

    updated url path

--- Install-Mods.ps1
+++ Install-Mods.ps1
@@ -185,7 +185,7 @@ function Install ($arguments) {
     # Download and install helmetcamera
     Write-Host "Downloading and installing HelmetCamera"
     $hcPath = Join-Path $lethalCompanyPath "BepInEx/plugins/HelmetCamera.dll/"
-    powershell -Command Invoke-WebRequest https://github.com/TwoCanOfTuna/company-mods-install/releases/download/company-mods-install3/HelmetCamera.dll -OutFile $hcPath
+    powershell -Command Invoke-WebRequest "https://github.com/TwoCanOfTuna/company-mods-install/releases/download/company-mods-install3/HelmetCamera.dll" -OutFile $hcPath
 }
 
 try {
